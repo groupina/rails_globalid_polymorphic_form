49e9e5ea582ad6ab797b66004de105406a0d4cd4
Shankar Dhanasekaran <shankar@opendrops.com>
2015-01-30 22:39:02 +0530

    Use virtual attributes to set and get global ids

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :owner, polymorphic: true
+
+  def global_owner
+    self.owner.to_global_id if self.owner.present?
+  end
+
+  def global_owner=(owner)
+    self.owner = GlobalID::Locator.locate owner
+  end
+  
 end
